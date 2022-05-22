ab29395de264460cac3c000bc586228da7ee9576
Mirzagayev Aydin <aydin.mirzagaev@gmx.de>
2022-05-22 16:38:01 +0200

    readme

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
