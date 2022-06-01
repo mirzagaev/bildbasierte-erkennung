b58d0a20291070d8ad70ede5deb79519a032d430
Mirzagayev Aydin <aydin.mirzagaev@gmx.de>
2022-06-01 16:00:15 +0200

    app is running

--- 02-react-app/src/App.js
+++ 02-react-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState, useEffect, useCallback } from "react";
 import Webcam from "react-webcam";
 import * as tf from '@tensorflow/tfjs';
-import './App.css';
+import './App.scss';
 
 function App() {
   const webcamRef = useRef(null);
   const [startCam, setStartCam] = useState(false);
   const [model, setModel] = useState();
-  const threshold = 0.75;
+  const [predictions, setPredictions] = useState([]);
+  const schwelle = 0.75;
   const modelURL = "http://127.0.0.1:8080/tfjs_plants_mobilenetv2_640/model.json";
   const classesDir = {
     1: {
@@ -34,73 +35,108 @@ function App() {
   }
 
   const detect = (stream) => {
-    tf.engine().startScope();
-    if ( stream !== undefined && stream !== null ) {
-      console.log("streamt...")
+    if (stream !== undefined && stream !== null) {
+      console.log("streamt...");
       model.executeAsync(process_input(stream)).then((predicts) => {
-        // console.log(predicts);
-
         renderDetections(predicts);
-
-        // predicts.map((predict) => {
-        //   console.log(predict);
-        // });
 
         requestAnimationFrame(() => {
           detect(stream);
         });
-        tf.engine().endScope();
       });
     }
-  };
+  }
 
   const process_input = (video_frame) => {
     const tfimg = tf.browser.fromPixels(video_frame).toInt();
-    // const tfimg = tf.browser.fromPixels(video_frame).resizeNearestNeighbor([224, 224]).toInt().expandDims();
     const expandedimg = tfimg.transpose([0,1,2]).expandDims();
     return expandedimg;
   };
 
   const renderDetections = (detections) => {
-    const predictions = detections[0].dataSync();
+    setPredictions([]);
     const predicted_index = detections[0].as1D().argMax().dataSync()[0];
     const predictedClass = classesDir[predicted_index];
-    // const score = predictions[predicted_index];
     const score = detections[1].dataSync()[0];
     const confidence = Math.round(score * 100);
-    const count = detections[5].dataSync()[0];
-    const boxes = detections[4].arraySync();
-    if ( score > threshold ) {
-      console.log("Count: ", count);
-      
-      console.log(predictedClass.name);
-      console.log("Score: ", confidence+"%");
+    // const count = detections[5].dataSync()[0];
+    const boxes = detections[7].arraySync();
+
+    const media = webcamRef?.current?.video;
+    const videoWidth = media.offsetWidth;
+    const videoHeight = media.offsetHeight;
+    
+    if ( score > schwelle ) {
+      const detectionObjects = []
+      const bbox = [];
+      const minY = boxes[0][0][0] * videoHeight;
+      const minX = boxes[0][0][1] * videoWidth;
+      const maxY = boxes[0][0][2] * videoHeight;
+      const maxX = boxes[0][0][3] * videoWidth;
+
+      bbox[0] = minX;
+      bbox[1] = minY;
+      bbox[2] = maxX - minX;
+      bbox[3] = maxY - minY;
+
+      detectionObjects.push({
+        class: predicted_index,
+        label: predictedClass.name,
+        score: confidence,
+        bbox: bbox
+      })
+
+      setPredictions(detectionObjects);
     }
   };
 
-  const onLoadedData = useCallback(() => {
+  const onLoadedData = () => {
     detect(webcamRef?.current?.video)
-  }, [startCam]);
+  };
 
   return (
     <section>
       { model && (
       <>
-        { startCam && (
         <>
           <div className="relative">
-            <Webcam
-              audio={false}
-              ref={webcamRef}
-              className="w-auto mx-auto"
-              screenshotFormat="image/jpeg"
-              videoConstraints={{
-                width: 1280,
-                height: 720,
-                facingMode: "environment"
-              }}
-              onLoadedData={onLoadedData}
-            />
+            { startCam && (
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                className="w-auto mx-auto"
+                screenshotFormat="image/jpeg"
+                videoConstraints={{
+                  width: 1280,
+                  height: 720,
+                  facingMode: "environment"
+                }}
+                onLoadedData={onLoadedData}
+              />
+            )}
+
+            {predictions.map((object, index) => (
+              <div
+                  key={index}
+                  className="bounding_boxes"
+                  style={{
+                      left: object.bbox[0]+'px',
+                      top: object.bbox[1]+'px',
+                      width: object.bbox[2]+'px',
+                      height: object.bbox[3]+'px'
+                  }}
+              >
+                  <p  className="px-3 py-2 uppercase bounding_boxes_class"
+                      style={{
+                          top: 0,
+                          left: 0,
+                          right: object.bbox[1]+'px',
+                          width: object.bbox[2]+'px'
+                      }}>{object.label} {object.score}%
+                  </p>
+              </div>
+              ))
+            }
           </div>
           <div className="fixed bottom-0 z-30 w-full">
             <div className="container flex flex-wrap m-auto">
@@ -110,13 +146,11 @@ function App() {
             </div>
           </div>
         </>
-        )}
-
         { !startCam && (
         <>
-          <section className = "container mx-auto bg-white" >
-            <div id = "startInfo" className = "container p-20 mx-auto text-center" >
-              <p id = "loadingInfo" >Komponenten sind geladen, Sie können die Anwendung starten. </p>
+          <section className="container mx-auto bg-white" >
+            <div id="startInfo" className="container p-20 mx-auto text-center" >
+              <p id="loadingInfo" >Komponenten sind geladen, Sie können die Anwendung starten. </p>
             </div>
           </section>
           <div className="fixed bottom-0 z-30 w-full">
@@ -130,11 +164,10 @@ function App() {
         )}
       </>
       )}
-
       { !model && (
-      <section className = "container mx-auto bg-white" >
-        <div id = "startInfo" className = "container p-20 mx-auto text-center" >
-          <p id = "loadingInfo" > Komponenten werden geladen, einen kurzen Augenblick...</p>
+      <section className="container mx-auto bg-white" >
+        <div id="startInfo" className="container p-20 mx-auto text-center" >
+          <p id="loadingInfo" > Komponenten werden geladen, einen kurzen Augenblick...</p>
         </div>
       </section>
       )}
